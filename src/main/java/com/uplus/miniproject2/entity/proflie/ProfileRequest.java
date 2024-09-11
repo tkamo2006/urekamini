a0e190ab520159b1d7752cf575822675a9485009
leedohyun <lucky77795@naver.com>
2024-09-11 16:38:55 +0900

    chore: getter 추가

--- src/main/java/com/uplus/miniproject2/entity/proflie/ProfileRequest.java
+++ src/main/java/com/uplus/miniproject2/entity/proflie/ProfileRequest.java
@@ -14,10 +14,12 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "profile_request")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProfileRequest {
 
