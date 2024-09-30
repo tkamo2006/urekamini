bb92f9eaf73b71a7af694ef2a198c2303334512d
KIMBUMSU123 <88920973+KIMBUMSU123@users.noreply.github.com>
2024-09-30 15:15:09 +0900

    그룹코드작업

---
+++ src/main/java/com/uplus/miniproject2/entity/common/Key/CodeKey.java
@@ -0,0 +1,23 @@
+package com.uplus.miniproject2.entity.common.Key;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+//@AllArgsConstructor 로 처리하려면 serialVersionUID 제거해야 그렇지 않으면 별도의 생성자 추가
+@NoArgsConstructor
+@Embeddable
+public class CodeKey implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String groupCode;
+    private String code;
+    
+	public CodeKey(String groupCode, String code) {
+		this.groupCode = groupCode;
+		this.code = code;
+	}
+}
